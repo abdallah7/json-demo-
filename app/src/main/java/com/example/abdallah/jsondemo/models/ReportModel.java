f4599c6ad049f1e0ad8f997787d915d68931f285
abdallah <abdallah.ramdan94@gmail.com>
2016-04-17 15:26:50 +0200

    edit name of list class

---
+++ app/src/main/java/com/example/abdallah/jsondemo/models/ReportModel.java
@@ -0,0 +1,29 @@
+package com.example.abdallah.jsondemo.models;
+
+/**
+ * Created by Abdallah on 3/24/2016.
+ */
+public class ReportModel {
+        private String Title;
+
+        private String image;
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String Title) {
+        this.Title = Title;
+    }
+
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+
+}
